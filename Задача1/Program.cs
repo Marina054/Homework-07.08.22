6921ef9fa7b4a46d69748f7c8f4445eaca3a4c99
marina <litvinova_06@bk.ru>
2022-08-07 23:23:24 +0500

    задача 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 1
+int a=5,
+    b=7; //max -b
+
+if(a<b)
+{
+    Console.WriteLine("Первое выражение");
+
+    Console.Write("max ");
+    Console.WriteLine(b);
+    Console.Write("min ");
+    Console.WriteLine(a);
+}
+
+int aa=2,
+    bb=10; //max -bb
+
+if(aa<bb)
+{
+    Console.WriteLine("Второе выражение.");
+
+    Console.Write("max ");
+    Console.WriteLine(bb);
+    Console.Write("min ");
+    Console.WriteLine(aa);
+}
+
+int aaa=-9,
+    bbb=-3; //max -bbb
+
+if(aaa<bbb)
+{
+    Console.WriteLine("Третье выражение.");
+
+    Console.Write("max ");
+    Console.WriteLine(bbb);
+    Console.Write("min ");
+    Console.WriteLine(aaa);
+}
+
+
+
+
+
+
