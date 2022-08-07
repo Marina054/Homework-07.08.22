8f2bf8a6c391261d601bf919a914c775f11b765f
marina <litvinova_06@bk.ru>
2022-08-08 01:05:40 +0500

    задача 3

---
+++ Задача1/Задача3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 3
+
+int a=4, //-> да
+       b=-3, // -> нет
+       c=7; //-> нет
+
+if (a % 2==0)
+            {
+                Console.WriteLine("число четное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+if (b % 2==0)
+            {
+                Console.WriteLine("число четное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+
+if (c % 2==0)
+            {
+                Console.WriteLine("число четное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }            
