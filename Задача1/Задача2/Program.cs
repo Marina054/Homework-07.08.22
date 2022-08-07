79b4f55ad2fbe037b80423b1631e2c12ef635319
marina <litvinova_06@bk.ru>
2022-08-07 23:57:38 +0500

    Задача 2

---
+++ Задача1/Задача2/Program.cs
@@ -0,0 +1,39 @@
+﻿// //Задача 2
+// int a=2,
+//     b=3,
+//     c=7; //Max -7
+
+// int max=a;
+
+// if(b>max) max=b;
+// if(c>max) max=c;
+
+// Console.Write("Max ");
+// Console.WriteLine(max);
+
+
+
+// int a=44,
+//     b=5,
+//     c=78; //Max -78
+
+// int max=a;
+
+// if(b>max) max=b;
+// if(c>max) max=c;
+
+// Console.Write("Max ");
+// Console.WriteLine(max);
+
+
+int a=22, //Max -22
+    b=3,
+    c=9; 
+
+int max=a;
+
+if(b>max) max=b;
+if(c>max) max=c;
+
+Console.Write("Max ");
+Console.WriteLine(max);
