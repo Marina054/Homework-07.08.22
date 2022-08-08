5cf877ffe5e431c422a4c95ff7a2f617a5835ee2
marina <litvinova_06@bk.ru>
2022-08-08 23:20:36 +0500

    задача 1 max and min

---
+++ Задача1/Задача11/Program.cs
@@ -0,0 +1,41 @@
+﻿int a = 30;
+int b = 15;
+
+int max =a;
+if (b>max) max=b;
+
+Console.Write("Max:");
+Console.WriteLine (max);
+
+int min=b;
+if (a<min) min =a;
+Console.Write("Min:");
+Console.WriteLine (min);
+
+int a1 = 2;
+int b1 = 10;
+
+int max1 =a1;
+if (b1>max1) max1=b1;
+
+Console.Write("Max:");
+Console.WriteLine (max1);
+
+int min1=b1;
+if (a1<min1) min1 =a1;
+Console.Write("Min:");
+Console.WriteLine (min1);
+
+int a2 = -9;
+int b2 = -3;
+
+int max2 =a2;
+if (b2>max2) max2=b2;
+
+Console.Write("Max:");
+Console.WriteLine (max2);
+
+int min2=b2;
+if (a2<min2) min2 =a2;
+Console.Write("Min:");
+Console.WriteLine (min2);
