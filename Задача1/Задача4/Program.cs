183dc3fe0d5775a0799e1b51706b636ee1829590
marina <litvinova_06@bk.ru>
2022-08-08 23:42:06 +0500

    задача 4 четные числа

---
+++ Задача1/Задача4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4.1
+int n = 5;
+    
+for (int i = 1; i <= n; i++)
+if (i % 2 == 0)
+ Console.WriteLine(i);
+
+
+//Задача 4.2
+
+int n = 8;
+
+for (int i = 1; i <= n; i++)
+if (i % 2 == 0)
+Console.WriteLine(i);
